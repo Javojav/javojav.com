7a63ee0f330a436ecf844bb7f1e258c802e47a2c
Javi <javipelema@gmail.com>
2017-02-12 16:35:55 +0100

    Update sketch.js

--- pong/sketch.js
+++ pong/sketch.js
@@ -16,8 +16,7 @@ function draw() {
   background(0);
   textSize(20);
   fill(255);
-  //text("©Javi", width-55, height-10);
-  text(Pb , width-55, height-10);
+  text("©Javi", width-55, height-10);
   //puntos
   textSize(100);
   fill(255);
