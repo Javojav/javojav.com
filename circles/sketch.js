eb53134d5bb0b8e110fcbf0efae9ffc159e38a53
ANyanCatFan <saitam.spatz@gmail.com>
2017-02-13 19:30:09 +0000

    Otra vez todo bonito y Bob se mueve todo el rato en lugar de cuando haces click

--- circles/sketch.js
+++ circles/sketch.js
@@ -1,17 +1,17 @@
- var circleX , circleY;
+var circleX , circleY;
+
 function setup() {
-  createCanvas(window.innerWidth-20,window.innerHeight-20);
+  createCanvas(windowWidth, windowHeight);
   background(0);
 }
 
 function draw() {
-  circleX=random(width);
-  circleY=random(height);
+  circleX = random(width);
+  circleY = random(height);
   fill(random(0, 255), random(0, 255), random(0, 255));
   ellipse(circleX, circleY, 50, 50);
 }
 
 function mousePressed() {
-   createCanvas(window.innerWidth,window.innerHeight);
-     background(0);
+  background(0);
 }
--- pong/sketch.js
+++ pong/sketch.js
@@ -2,7 +2,7 @@ var BY, BX, T, BDY, BDX;
 var Pb, Bot;
 var PtsA = 0, PtsB = 0;
 function setup() {
-  background(0); 
+  background(0);
   createCanvas(1080, 720);
   BY = height/2;
   BX = width/2;
@@ -17,29 +17,34 @@ function draw() {
   textSize(20);
   fill(255);
   text("©Javi", width-55, height-10);
+
   //puntos
   textSize(100);
   fill(255);
   text(PtsA, width*0.25, height/2);
   text(PtsB, width*0.75, height/2);
+
   //jugadores
   fill(255);
   rect(width-15, mouseY, 15, 60);
   rect(0, Pb, 15, 60);
+
   //linea centro
   rect(width/2-5, 0, 10, height);
+
   //pelota
   fill(150, 0, 0);
   ellipse(BX, BY, 50, 50);
+
   //bot
   if (Pb < 0) {
-   Pb = 2; 
+   Pb = 2;
   }
-  
+
   if (Pb > height-60) {
-   Pb = height-62; 
+   Pb = height-62;
   }
-  
+
   if (BX < width/2) {
     if (Bot == 0) {
       Pb = BY - 30;
@@ -55,6 +60,7 @@ function draw() {
       }
     }
   }
+
   //direccion pelota
   if (BDX == 1) {
     BX = BX - 3;
@@ -62,12 +68,14 @@ function draw() {
   if (BDX == 2) {
     BX = BX +3;
   }
+
   //pelota arriba/abajo
   BY = BY - BDY;
-  //choque 
+
+  //choque
   if (BY < 25 || BY > height-25) {
     BDY = BDY * -1;
-  } 
+  }
   if (BX > width-25 && BY < mouseY + 60 && BY > mouseY) {
     BDX = 1;
     BDY = random(-7, 7);
@@ -80,6 +88,7 @@ function draw() {
     BDY = random(-7, 7);
     Bot = 1;
   }
+
   //reset
   if (BX == 0) {
     PtsB = PtsB + 1;
