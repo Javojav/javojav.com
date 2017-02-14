968f5881ec637f44f1bba23e6fa3418d6d39eef5
Javi <javipelema@gmail.com>
2017-02-14 18:09:14 +0100

    Update sketch.js

--- pong/sketch.js
+++ pong/sketch.js
@@ -1,36 +1,39 @@
 var BY, BX, T, BDY, BDX;
 var Pb, Bot;
-var PtsA = 0, PtsB = 0;
+var PtsA = 0,
+  PtsB = 0;
+
 function setup() {
   background(0);
   createCanvas(1080, 720);
-  BY = height/2;
-  BX = width/2;
+  BY = height / 2;
+  BX = width / 2;
   BDY = random(-7, 7);
   BDX = 2;
   T = random(1, 10);
-  Pb = height/2-15;
+  Pb = height / 2 - 15;
+  slider = createSlider(0, 20, 3, 0, 3, 6, 10,15,20);
 }
 
 function draw() {
   background(0);
   textSize(20);
   fill(255);
-  text("©Javi", width-55, height-10);
+  text("©Javi", width - 55, height - 10);
 
   //puntos
   textSize(100);
   fill(255);
-  text(PtsA, width*0.25, height/2);
-  text(PtsB, width*0.75, height/2);
+  text(PtsA, width * 0.25, height / 2);
+  text(PtsB, width * 0.75, height / 2);
 
   //jugadores
   fill(255);
-  rect(width-15, mouseY, 15, 60);
+  rect(width - 15, mouseY, 15, 60);
   rect(0, Pb, 15, 60);
 
   //linea centro
-  rect(width/2-5, 0, 10, height);
+  rect(width / 2 - 5, 0, 10, height);
 
   //pelota
   fill(150, 0, 0);
@@ -38,24 +41,24 @@ function draw() {
 
   //bot
   if (Pb < 0) {
-   Pb = 2;
+    Pb = 2;
   }
 
-  if (Pb > height-60) {
-   Pb = height-62;
+  if (Pb > height - 60) {
+    Pb = height - 62;
   }
 
-  if (BX < width/2) {
+  if (BX < width / 2) {
     if (Bot == 0) {
       Pb = BY - 30;
     } else {
       if (Pb > BY) {
-        Pb = Pb - T;
+        Pb = Pb - T + slider.value();
       }
       if (Pb < BY) {
-        Pb = Pb + T;
+        Pb = Pb + T + slider.value();
       }
-      if (Pb < BY + 30 && Pb > BY -30) {
+      if (Pb < BY + 30 && Pb > BY - 30) {
         Bot = 0;
       }
     }
@@ -63,24 +66,24 @@ function draw() {
 
   //direccion pelota
   if (BDX == 1) {
-    BX = BX - 3;
+    BX = BX - slider.value();
   }
   if (BDX == 2) {
-    BX = BX +3;
+    BX = BX + slider.value();
   }
 
   //pelota arriba/abajo
   BY = BY - BDY;
 
   //choque
-  if (BY < 25 || BY > height-25) {
+  if (BY < 25 || BY > height - 25) {
     BDY = BDY * -1;
   }
-  if (BX > width-25 && BY < mouseY + 60 && BY > mouseY) {
+  if (BX > width - 25 && BY < mouseY + 60 && BY > mouseY) {
     BDX = 1;
     BDY = random(-7, 7);
-    T = T -1
-      Bot = 1;
+    T = T - 1
+    Bot = 1;
   }
   if (BX < 25 && BY < Pb + 60 && BY > Pb) {
     BDX = 2;
@@ -90,18 +93,18 @@ function draw() {
   }
 
   //reset
-  if (BX == 0) {
+  if (BX < 0) {
     PtsB = PtsB + 1;
-    BY = height/2;
-    BX = width/2;
+    BY = height / 2;
+    BX = width / 2;
     BDY = random(-7, 7);
     BDX = 2;
     T = random(1, 10);
   }
-  if (BX == width) {
+  if (BX > width) {
     PtsA = PtsA + 1;
-    BY = height/2;
-    BX = width/2;
+    BY = height / 2;
+    BX = width / 2;
     BDY = random(-7, 7);
     BDX = 2;
     T = random(1, 10);
