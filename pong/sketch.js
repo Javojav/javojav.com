7f191f2ec3439d342e2cc61e3eebbfa69175ab93
Javi <javipelema@gmail.com>
2017-02-15 16:47:20 +0100

    Update sketch.js

--- pong/sketch.js
+++ pong/sketch.js
@@ -13,6 +13,7 @@ function setup() {
   T = random(1, 10);
   Pb = height / 2 - 15;
   slider = createSlider(0, 20, 3, 0, 3, 6, 10,15,20);
+  a = createA(href="/pong","Pong");
 }
 
 function draw() {
