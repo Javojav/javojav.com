45fcd398a6553c24ed62cded99fde8c386630711
Javi <javipelema@gmail.com>
2017-03-28 19:08:03 +0200

    Update sketch.js

--- PongBot/sketch.js
+++ PongBot/sketch.js
@@ -15,10 +15,12 @@ function setup() {
   Pa = height / 2 - 15;
   slider = createSlider(0, 20, 3, 0, 3, 6, 10, 15, 20);
   a = createA(href = "/pong", "Pong");
+  a.style("font-family", "'Rajdhani', sans-serif");
   a = createA(href = "", " ");
   a.style("color", "white");
   a = createA(href = "/Pong2P", "2P");
   a.style("color", "purple");
+  a.style("font-family", "'Rajdhani', sans-serif");
 }
 
 function draw() {
