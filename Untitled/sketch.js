159c48b2a87008d1c7ac7046fe8863493a5387b6
Javi <javipelema@gmail.com>
2017-04-13 21:21:14 +0200

    Added Bosses, lives, Bug fixes and level displayed

--- Untitled/sketch.js
+++ Untitled/sketch.js
@@ -5,9 +5,10 @@ var px = 100,
 	plus = true,
 	shot = [],
 	zombie = [],
+	boss = [],
 	look = 1,
 	level = 0,
-	stop = false,
+	vida = 3,
 	wait = 101,
 	reloadx = 23,
 	reloady = 20,
@@ -22,35 +23,34 @@ var px = 100,
 	gun = true,
 	bag = [],
 	auto = 30,
-	next = 0;
-
+	next = 0,
+	invencible = 255,
+	howmanybosses = 0;
 
 function setup() {
 	createCanvas(800, 600);
-	bag.push(new Bag());
-	bag[0].x = 10000;
-	bag[0].y = 10000;
 }
 
 function draw() {
-	if (stop == false) {
+	if (vida > -1) {
 		lugar();
+		invencible++;
 		player(px, py);
 		reloadgun();
 		reloadak();
 		wait++;
-		if (ammo.total < 0 && ammo.ingun < 0) {
-			ammo.ingun = 0;
-			ammo.total = 0;
-		}
-		if (akammo.total < 0 && akammo.ingun < 0) {
-			akammo.ingun = 0;
-			akammo.total = 0;
-		}
+		if (ammo.total < 0) ammo.total = 0;
+		if (akammo.total < 0) akammo.total = 0;
+		if (ammo.ingun < 0) ammo.ingun = 0;
+		if (akammo.ingun < 0) akammo.ingun = 0;
+
 		fill(255)
 		textSize(10);
-		text(ammo.ingun + "/" + ammo.total, width - 100, height - 10);
-		text(akammo.ingun + "/" + akammo.total, width - 200, height - 10);
+		text(ammo.ingun + "/" + ammo.total, width - 75, height - 10);
+		text(akammo.ingun + "/" + akammo.total, width - 150, height - 10);
+		text("level: " + level, 10, height - 10);
+		text("X " + vida, width - 200, height - 10);
+		head(width - 208, height - 13.5);
 		if (keyIsDown(UP_ARROW) && py > 45) {
 			py -= 2;
 			look = 3;
@@ -119,7 +119,12 @@ function draw() {
 			zombie[i2].display();
 			zombie[i2].die();
 		}
-		if (zombie.length < 1 && next == 2) {
+		for (var i6 = 0; i6 < boss.length; i6++) {
+			boss[i6].move();
+			boss[i6].display();
+			boss[i6].die();
+		}
+		if (zombie.length < 1 && next == 2 && boss.length < 1) {
 			next = 0;
 		}
 		if (next == 0) {
@@ -132,8 +137,19 @@ function draw() {
 		}
 		if (next == 1) {
 			px = 100;
-			for (var i3 = 0; i3 < level; i3++) {
-				zombie.push(new Zombie())
+			if (level != 5) {
+				if (level % 5 === 0) howmanybosses++;
+				for (var i7 = 0; i7 < howmanybosses; i7++) {
+					boss.push(new Boss())
+					next = 2;
+				}
+				for (var i3 = 0; i3 < level; i3++) {
+					zombie.push(new Zombie())
+					next = 2;
+				}
+			}
+			if (level == 5) {
+				boss.push(new Boss())
 				next = 2;
 			}
 		}
@@ -141,8 +157,14 @@ function draw() {
 			bag[i4].display();
 			for (var i5 = bag.length - 1; i5 > -1; i5--) {
 				if (px < bag[i5].x + 20 && px > bag[i5].x - 20 && py < bag[i5].y + 20 && py > bag[i5].y - 50) {
-					if (bag[i5].boatopespepesial > 7.5) akammo.total += bag[i5].howmuch;
-					if (bag[i5].boatopespepesial < 7.5) ammo.total += bag[i5].howmuch;
+					if (bag[i5].live == false) {
+						if (bag[i5].boatopespepesial > 7.5) akammo.total += bag[i5].howmuch;
+						if (bag[i5].boatopespepesial < 7.5) ammo.total += bag[i5].howmuch;
+					}
+					if (bag[i5].live == true) {
+						vida++;
+					}
+					i4--;
 					bag.splice(i5, 1);
 				}
 			}
@@ -150,7 +172,19 @@ function draw() {
 		if (a > 3) plus = false;
 		if (a < -3) plus = true;
 	}
-	if (stop == true) button();
+	if (vida < 0) button();
+}
+
+function head(x, y) {
+	strokeWeight(.5)
+	stroke(255);
+	fill(0);
+	ellipse(x, y, 10, 10);
+	fill(255);
+	ellipse(x - 3, y - 1, 1);
+	ellipse(x + 3, y - 1, 1);
+	strokeWeight(1);
+	stroke(0);
 }
 
 function lugar() {
@@ -222,12 +256,12 @@ function lugar() {
 function player(px, py) {
 	stroke(0);
 	//noStroke();
-	fill(1);
+	fill(0, invencible)
 	ellipse(px, py, 30, 30);
-	fill(255);
+	fill(255, invencible);
 	if (look == 1) {
 		ellipse(px + 7, py - 2, 5, 5);
-		fill(0);
+		fill(0, invencible)
 		rect(px - 6, py + 18, 12, 20);
 		strokeWeight(3);
 		line(px, py + 23, px + 12, py + 23);
@@ -250,7 +284,7 @@ function player(px, py) {
 	}
 	if (look === 0) {
 		ellipse(px - 7, py - 2, 5, 5);
-		fill(0);
+		fill(0, invencible)
 		rect(px - 6, py + 18, 12, 20);
 		strokeWeight(3);
 		line(px, py + 23, px - 12, py + 23);
@@ -274,7 +308,7 @@ function player(px, py) {
 	if (look == 2) {
 		ellipse(px - 6, py - 2, 5, 5);
 		ellipse(px + 6, py - 2, 5, 5);
-		fill(0);
+		fill(0, invencible);
 		rect(px - 8, py + 18, 16, 20);
 		strokeWeight(3);
 		line(px - 5, py + 39, px - 5, py + 45 + a);
@@ -282,7 +316,7 @@ function player(px, py) {
 		strokeWeight(1);
 	}
 	if (look == 3) {
-		fill(0);
+		fill(0, invencible)
 		rect(px - 8, py + 18, 16, 20);
 		strokeWeight(3);
 		line(px - 5, py + 39, px - 5, py + 45 + a);
@@ -339,6 +373,103 @@ function reloadak() {
 			}
 		}
 	}
+}
+
+function Boss() {
+	this.x = round(random(100, width - 100));
+	this.live = 5;
+	this.livebar = 40 / this.live
+	this.plus = true;
+	this.za = -5;
+	this.zb = 5;
+	this.y = round(random(100, height - 100));
+	while (px < this.x + 100 && px > this.x - 100 && py < this.y + 100 && py > this.y - 100) {
+		this.x = round(random(100, width - 100));
+		this.y = round(random(100, height - 100));
+	}
+	this.display = function() {
+		bos(this.x, this.y, this.dir, this.za, this.zb);
+		fill(0)
+		rect(this.x - 20, this.y - 50, 40, 10);
+		fill(255)
+		rect(this.x - 20, this.y - 50, this.live * this.livebar, 10);
+	};
+	this.move = function() {
+		if (px > this.x) {
+			this.speedx = 0.6;
+			this.dir = 1;
+		}
+		if (px == this.x) this.speedx = 0;
+		if (px < this.x) {
+			this.speedx = -0.6;
+			this.dir = 0;
+		}
+		if (py > this.y) {
+			this.speedy = 0.6;
+		}
+		if (py == this.y) this.speedy = 0;
+		if (py < this.y) {
+			this.speedy = -0.6;
+		}
+		if (this.x > width - (75 + 15 * 2)) this.x--;
+		if (this.x < 75 + 15 * 2) this.x++;
+		if (this.y > height - (75 + 50 * 2)) this.y--;
+		if (this.plus == true) {
+			this.za += 1;
+			this.zb -= 1;
+		} else {
+			this.zb += 1;
+			this.za -= 1;
+		}
+		if (this.za < -5) this.plus = true;
+		if (this.za > 5) this.plus = false;
+
+		this.x += this.speedx;
+		this.y += this.speedy;
+	};
+	this.die = function() {
+		if (px < this.x + 15 * 2 && px > this.x - 15 * 2 && py < this.y + 55 * 2 && py > this.y - 30 * 2) {
+			if (invencible > 255) {
+				invencible = 100;
+				vida--;
+			}
+		}
+	};
+}
+
+function bos(bx, by, blook, ba, bb) {
+	//stroke(0);
+	noStroke();
+	fill(200, 0, 0);
+	ellipse(bx, by, 30 * 2, 30 * 2);
+	fill(255);
+	if (blook == 1) {
+		ellipse(bx + 7 * 2, by - 2 * 2, 5 * 2, 5 * 2);
+		fill(200, 0, 0);
+		rect(bx - 6 * 2, by + 18 * 2, 12 * 2, 20 * 2);
+		strokeWeight(3 * 2);
+		stroke(0);
+		line(bx + 8 * 2, by + 23 * 2, bx + 15 * 2, by + 23 * 2);
+		line(bx - 2 * 2, by + 39 * 2, bx - 2 * 2 + ba * 2, by + 44 * 2);
+		line(bx + 2 * 2, by + 39 * 2, bx + 2 * 2 + bb * 2, by + 44 * 2);
+		strokeWeight(1);
+	}
+	if (blook === 0) {
+		ellipse(bx - 7 * 2, by - 2 * 2, 5 * 2, 5 * 2);
+		fill(200, 0, 0);
+		noStroke();
+		rect(bx - 6 * 2, by + 18 * 2, 12 * 2, 20 * 2);
+		stroke(0);
+		strokeWeight(3 * 2);
+		line(bx - 8 * 2, by + 23 * 2, bx - 15 * 2, by + 23 * 2);
+		line(bx + 2 * 2, by + 39 * 2, bx + 2 * 2 + ba * 2, by + 44 * 2);
+		line(bx - 2 * 2, by + 39 * 2, bx - 2 * 2 + bb * 2, by + 44 * 2);
+		strokeWeight(1);
+	}
+	noStroke();
+	fill(200, 0, 0);
+	rect(bx - 2 * 2, by + 10 * 2, 4 * 2, 8 * 2);
+	stroke(0);
 }
 
 function zom(zx, zy, zlook, za, zb) {
@@ -421,7 +552,10 @@ function Zombie() {
 	};
 	this.die = function() {
 		if (px < this.x + 15 && px > this.x - 15 && py < this.y + 15 && py > this.y - 55) {
-			stop = true;
+			if (invencible > 255) {
+				vida--;
+				invencible = 100;
+			}
 		}
 	};
 }
@@ -468,9 +602,23 @@ function Shot() {
 				if (bagyesorno > 7.5) {
 					bag.push(new Bag());
 					bag[bag.length - 1].x = zombie[i].x;
+					bag[bag.length - 1].live = false;
 					bag[bag.length - 1].y = zombie[i].y;
 				}
 				zombie.splice(i, 1);
+			}
+		}
+		for (var i2 = 0; i2 < boss.length; i2++) {
+			if (this.x < boss[i2].x + 15 * 2 && this.x > boss[i2].x - 15 * 2 && this.y < boss[i2].y + 43 * 2 && this.y > boss[i2].y - 15 * 2) {
+				this.hit = 1;
+				boss[i2].live--;
+			}
+			if (boss[i2].live < 1) {
+				bag.push(new Bag());
+				bag[bag.length - 1].live = true;
+				bag[bag.length - 1].x = boss[i2].x;
+				bag[bag.length - 1].y = boss[i2].y;
+				boss.splice(i2, 1);
 			}
 		}
 	};
@@ -481,15 +629,18 @@ function Bag() {
 	this.boatopespepesial = round(random(10));
 	this.display = function() {
 		stroke(0);
+		if (this.live == false) {
+			if (this.boatopespepesial > 7.5) fill(0, 0, 100);
+			else fill(0)
+			rect(this.x, this.y, 12, 20);
 
-		if (this.boatopespepesial > 7.5) fill(0, 0, 100);
-		else fill(0)
-		rect(this.x, this.y, 12, 20);
-		stroke(255);
-		strokeWeight(2)
-		line(this.x + 3, this.y + 2, this.x + 3, this.y + 16)
-		line(this.x + 6, this.y + 2, this.x + 6, this.y + 16)
-		line(this.x + 9, this.y + 2, this.x + 9, this.y + 16)
-		strokeWeight(1)
+			stroke(255);
+			strokeWeight(2)
+			line(this.x + 3, this.y + 2, this.x + 3, this.y + 16)
+			line(this.x + 6, this.y + 2, this.x + 6, this.y + 16)
+			line(this.x + 9, this.y + 2, this.x + 9, this.y + 16)
+			strokeWeight(1)
+		}
+		if (this.live == true) head(this.x, this.y);
 	};
 }
